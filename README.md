38ce16eb37393fc16ec3cca8a15ae4137f0cd6fe
zzz <zzz@zzz>
2022-04-10 22:35:49 +0800

    2022.04.10-init

---
+++ README.md
@@ -0,0 +1,61 @@
+
+#说明
+从2022年3月开始，上海经历了奥密克戎的袭击。在大学校老师的工作负担也直线上升，其中有一项工作是收集所有学生的核酸检查结果，手工统计非常耗费精力。白天要尽力服务学院几百位学生，晚上还学处理这些耗费精力的事情。
+
+老师们的身体渐渐吃不消。
+
+作为一名老师家属，当得知还有这种统计工作，首先想到的是为什么随身办（从2022年04月09日起切换为随身办核酸码）的开发者，不开发这样的功能？竟让广大基层工作者浪费如此大量的人力物力去做统计工作。 真的希望负责研发随身办的人员能主动听一听基层人员的建议或苦衷。那才是我们不断创新、进步的源泉。
+
+但是，我的力量有限，只能“将计就计”，那就利用现有paddleocr的中文识别技术去识别所有同学的截图来一定程度的自动化统计。
+
+希望这个简单的程序能缓解基层工作人员的压力，也希望开发者能主动收集建议，而不是闭门造车。
+
+运行时间：~s/picture
+
+不可避免存在bug，欢迎评论。
+
+# 安装
+```
+git clone 
+cd check_acid_test
+conda create -name ocr
+conda activate ocr
+conda install python==3.8
+pip install -r requirements.txt
+```
+
+# 数据准备
+
+* 放置所有的照片在images文件夹中，如：
+![img.png](utils/img1.png)
+* 放置学生信息统计表name_list.xlsx，如
+![img.png](utils/img7.png)
+文件中也可以不包含证件号码信息，此时则无法处理重名的情况，需手工查验的需求增加
+![img.png](utils/img2.png)
+
+* 在check_acid_test 目录中运行命令
+```
+python main_ssb.py
+```
+你也可以指定图片文件夹和name_list.xlsx位置
+
+```
+python main_ssb.py --names C:\check_acid_test\backup\name_list.xlsx --images C:\check_acid_test\backup\images
+```
+
+得到结果：
+
+![img.png](utils/img3.png)
+
+* 同一目录下，会多出一个```name_list_{当前时间}.xlsx```的文件
+
+在该文件的overview sheet中，会多出4列，test列记录了学生是否已经做核酸（1是做了核酸），img列记录了与该学生匹配的图片名字，其他3列根据列名即可知晓含义。
+
+![img.png](utils/img4.png)
+
+在img_detection_res sheet中，存储了图片检测结果， matching列，1代表了与输入的name_list.xlsx名单信息能匹配上。
+
+![img.png](utils/img5.png)
+
+在warning sheet中的3列分别记录了：需要提醒做核酸的同学名单，检测失败的图片名单（需要手工查验），检测成功但是与名单不符合的图片名单（需要手工查验）。
+![img.png](utils/img6.png)
---
+++ main_ssb.py
@@ -0,0 +1,155 @@
+'''
+    解析随身办的截图
+'''
+import pandas as pd
+import os
+import datetime
+import argparse
+from tqdm import tqdm
+from paddleocr import PaddleOCR
+
+def parse_args():
+    parser = argparse.ArgumentParser('training')
+    parser.add_argument('--names', type=str, default=r'name_list.xlsx', help='select name_list.xlsx')
+    parser.add_argument('--images', type=str, default=r'images', help='select image folder')
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    # reader = easyocr.Reader(['ch_sim'], gpu=True) # this needs to run only once to load the model into memory
+
+    excel_file = args.names
+    img_folder = args.images
+    print(f"You select -- file: {args.names},  image folder:  {args.images}")
+
+    # 读取所有用户信息
+    all_students = pd.read_excel(excel_file)
+    has_id_info = '证件号码' in all_students.columns
+
+    if has_id_info:
+        all_students[["证件号码"]] = all_students[["证件号码"]].astype(str)
+    else:
+        print('\033[1;31;40m Warning： The file does not include 证件号码 column！\033[0m')
+    all_students['test'] = 0  # 标记用户是否已经测试
+    all_students['采样时间'] = ''
+    all_students['检测结果'] = ''
+    all_students['img'] = ''  # 该用户匹配的图片
+
+    # 存储特殊用户信息
+    ls_det_failed = []
+    ls_not_matching = []
+    ls_warning = []
+
+    ls = ['姓名', '证件号码', '采样时间：', '检测结果：']
+    data = pd.DataFrame(columns=ls + ['img'])  # 添加一列记录img name
+    imgs = os.listdir(img_folder)
+
+    ocr = PaddleOCR(lang='ch', det_db_box_thresh=0.5)  #,
+                            # det_model_dir=r'./model/det/ch/ch_PP-OCRv2_det_infer',
+                            # rec_model_dir=r'./model/rec/ch/ch_PP-OCRv2_rec_infer',
+                            # cls_model_dir=r'./model/cls/ch_ppocr_mobile_v2.0_cls_infer')  # need to run only once to download and load model into memory
+
+    for img_path in tqdm(imgs):
+        # 遍历每一张图片
+        # img = imgs[0]
+        # paddleocr
+        result_o = ocr.ocr(os.path.join(img_folder, img_path), cls=False)
+        result = [line[1][0] for line in result_o]
+        # continue
+        # easyocr
+        # result = reader.readtext(os.path.join(img_folder, img_path), detail = 0)
+        cur = []
+        for col in ls:
+            # col = '姓名'
+            try:
+                ind_ls = result.index(col)   # 只查找第一个
+                item = result[ind_ls + 1]
+                # ind_ls_end = result.index(col_end)  # 避免检测出杂项,也寻找该项的下一项
+                # item = ''.join(result[ind_ls + 1: ind_ls_end])
+
+            except (ValueError, IndexError):
+                # 检测结果这一项，容易出问题，特地再次检测
+                if col == '检测结果：':
+                    for i in result:
+                        if '【' in i or '】' in i:
+                            item = i
+                            break
+
+                else:
+                    ls_det_failed.append(img_path)
+                    continue
+            cur.append(item)
+
+        # # 如果检测失败
+        # if len(cur) != len(ls):
+        #     ls_det_failed.append(img_path)
+        #     continue
+
+        # 修正日期：日与时之间无空格
+        if cur[2][10] != ' ':
+            hour_start = cur[2].find(':') - 2
+            cur[2] = cur[2][:hour_start] + ' ' + cur[2][hour_start:]
+
+        # 加入dataframe
+        data = data.append(dict(zip(ls + ['img'], cur + [img_path])), ignore_index=True)
+
+    data['matching'] = 0
+
+    # 开始匹配
+    for ind_img, row in data.iterrows():
+        # ind, row = next(data.iterrows())
+        name = row['姓名']
+        id = row['证件号码']
+
+        if has_id_info:
+            # 匹配id后4位
+            ind_id_postfix = all_students['证件号码'].str.endswith(id[-4:])
+        # 匹配name_postfix
+        name_postfix = name.split('*')[-1]
+        ind_name_postfix = all_students['姓名'].str.endswith(name_postfix)
+
+        # 以下每一项中，有且只有一行符合就算符合。有一项符合即可算匹配成功
+        # ind_id_postfix： 证件号码后四位匹配，且仅匹配到一位用户
+        # ind_name_postfix： 姓名匹配，且仅匹配到一位用户
+        if has_id_info:
+            matching_ls = [ind_id_postfix, ind_name_postfix]
+        else:
+            matching_ls = [ind_name_postfix]
+
+        for ind in matching_ls:
+            if ind.sum() == 1:
+                # 设置该用户已经测试核酸，并附上图片名称
+                all_students.loc[ind.to_list().index(True), 'test'] = 1
+                all_students.loc[ind.to_list().index(True), 'img'] = row['img']
+                all_students.loc[ind.to_list().index(True), '采样时间'] = row['采样时间：']
+                all_students.loc[ind.to_list().index(True), '检测结果：'] = row['检测结果：']
+
+                # 设置该img已经匹配
+                data.loc[ind_img, 'matching'] = 1
+                break
+        if data.loc[ind_img, 'matching'] != 1:  # 匹配失败
+            ls_not_matching.append(row['img'])
+
+    # 查找还没有测试的用户
+    ls_warning = all_students.loc[all_students['test'] == 0, '姓名']
+
+    print('\033[1;31;40m Detect failed imgs:\033[0m')
+    print(ls_det_failed)
+    print('\033[1;31;40m Detected, but not found in excel file:\033[0m')
+    print(ls_not_matching)
+    print('\033[1;31;40m These students deserve a warning:\033[0m')
+    print(list(ls_warning))
+
+    # 导出文件
+    output = f"{os.path.basename(args.names).split('.')[0]}_{datetime.datetime.now().strftime('%Y%m%d%H%M')}.xlsx"
+    with pd.ExcelWriter(output) as writer:
+        all_students.to_excel(writer, sheet_name='overview')
+        data.to_excel(writer, sheet_name='img_detection_res')
+        pd.DataFrame({'warning': [list(ls_warning)], 'imgs detect failed': [ls_det_failed], 'detecte successfully but imgs not matching': [ls_not_matching]}).to_excel(writer, sheet_name='warning')
+    print('Done.')
+
+    # 祝福
+    print('Keep healthy! Good luck to everyone.')
+
+if __name__ == '__main__':
+    main()
